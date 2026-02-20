dc20f01cd92ea03460c3cf0767566edf0eca8274
nmukassa1 <nmukassa1@gmail.com>
2026-02-20 15:11:34 +0000

    .

--- config/database.php
+++ config/database.php
@@ -31,10 +31,10 @@ return [
 
     'connections' => [
 
-        'pgsql' => [
-            'driver' => 'pgsql',
+        'sqlite' => [
+            'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.pgsql')),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
